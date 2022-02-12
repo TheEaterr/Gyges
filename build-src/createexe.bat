7a04b92490660fcad8b23428f6641828657b3fe2
Paul Breuil <tykapl.breuil@gmail.com>
2022-02-12 21:04:22 +0100

    Fix JRE

--- build-src/createexe.bat
+++ build-src/createexe.bat
@@ -1,5 +1,12 @@
-rmdir output\Gyges /s /q
+rmdir output /s /q
+md output
 md output\Gyges
 md output\Gyges\assets
+md output\Gyges\jre
 copy ..\assets output\Gyges\assets
+xcopy /s "C:\Users\tykap\AppData\Local\Programs\Eclipse Adoptium\jdk-17.0.2.8-hotspot\" output\Gyges\jre
 "C:\Program Files (x86)\Launch4j\launch4jc.exe" launch4jconfig.xml
+cd output\Gyges
+"C:\Program Files\7-Zip\7z.exe" a -tzip ..\Gyges.zip -r *.* -mx5
+cd ..
+rmdir Gyges /s /q
